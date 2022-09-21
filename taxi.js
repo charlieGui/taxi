19a71e89aab66cfee63965a7c6cba7da43239ae9
Guillaume <103214559+charlieGui@users.noreply.github.com>
2022-09-21 22:12:06 +0200

    Add files via upload

--- taxi.js
+++ taxi.js
@@ -1,6 +1,16 @@
 "use strict"
 
 $(document).ready(function () {
-    $('#intro').fadeIn(7000);
     $('.coming').fadeIn(5000);
+  soonAnimation();
+   
 });
+
+
+function soonAnimation(){
+    $('#intro').animate({"left" : "-70%"}, 25000, "linear",
+    function(){
+        $(this).css({"left": "100%"});
+    });
+    soonAnimation();
+}
