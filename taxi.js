1011e7587442110d0954358afd5c0cf6b2183aa6
Guillaume <103214559+charlieGui@users.noreply.github.com>
2022-09-24 15:57:28 +0200

    Add files via upload

--- taxi.js
+++ taxi.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
 
 
 function soonAnimation(){
-   if($(window).width() > 500){
-       // Animation lorsque l'ecran au dessus de 500px
-        $('#intro').animate({"left" : "-100%"}, 25000, "linear",
-        function(){
-            $(this).css({"left": "100%"});
-        });
-    }else if($(window).width() < 500){  //Animation lorsque l'écran est au-dessous de 5à00px
+   if($(window).width() > 680){
+       // Animation lorsque l'ecran au dessus de 1024px
+       $('#intro').animate({"left" : "-100%"}, 25000, "linear",
+       function(){
+           $(this).css({"left": "100%"});
+       });
+    }else if($(window).width() < 680){  //Animation lorsque l'écran est au-dessous de 5à00px
         $('#intro').animate({"left" : "-200%"}, 15000, "linear",
         function(){
             $(this).css({"left": "100%"});
